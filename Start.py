fc8b8e7cdfd1ca686e8cfb17d0e74deb78085fa9
nraturi9796 <152674283+nraturi9796@users.noreply.github.com>
2024-06-27 18:28:26 +0000

    Let's Stat

---
+++ Start.py
@@ -0,0 +1,59 @@
+# name="Anjali Rana"
+# age = 23
+# location="gahar";
+# print("she is my beautiful girl : ",  name)
+# print("her age is: ",age)
+# print("she is living in : " ,location)
+
+#what is pyhton
+# python is high level programming language ,
+# which is used in AI,Data Analyst,game,application;
+
+#features of pyhton
+# 1:high level langauage
+# 2:simple and easy Syntax
+# 3:interpretted
+# 4:Dynamic in nature
+# 6:open source
+# 7:OOPS
+# 8:Big library function
+# 9:plateform independent
+
+# first code of python
+# print("hello python")
+
+# variable->variable are the name which are given the values.
+# Values->Values are content which is store in a variable.
+#EX->
+# name="Anjali"
+# age=23
+# print("name is:",name)
+# print("age is:",age)
+#  so name is "variable" and Anjali is "Value"
+#identifiers->identifiers are combination of uppercase & lowercase letter,digit
+# we can not use % @ in this type of word in case of variable.
+# An idendifier are not start from digit Ex: 1age×
+#age4√ valid identifier
+
+#how to cheack type of variable->
+# name="Nitesh"
+# age=23
+# RoomNo=45.5
+# print(type(name))
+# print(type(age))
+# print(type(RoomNo))
+
+#Data Type
+#Integer->include this values + - and 0 in int we can not write -5.55 but we can write -5
+# String-> We can write world with in string.Like "Hello","Anjali" "Nitesh".
+#we can write string in single quote like"hello",and double quote''hello'',and tripale quote'''hello'''
+#Examle->
+# name1="AR"
+# name2='NR'
+# name3='''NR'''
+# print(name1)
+# print(name2)
+# print(name3)
+
+
+
